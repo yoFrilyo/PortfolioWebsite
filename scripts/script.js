b0b84580624bf2f63d7c4547dfa9f911828916a2
Owen Gallegos <owengallegos23@gmail.com>
2025-05-16 23:08:33 -0700

    Started list functionality for portfolio section

--- scripts/script.js
+++ scripts/script.js
@@ -4,8 +4,22 @@
 fetch('../content/index-content.json')
     .then(response => response.json())
     .then(data => {
+        // hero
+        document.getElementById('hero-helper').innerHTML = data.hero.helper
+        document.getElementById('name').innerHTML = data.hero.title;
+
+        //about
         const aboutText = data.about.text.replace(/\n/g, '<br>')
         document.getElementById('bio').innerHTML = aboutText;
+
+        // portfolio
+        const container = document.getElementById('project-list');
+        data.portfolio.forEach(project => {
+            const item = document.createElement('li');
+            item.className = 'project';
+            item.innerHTML = `<h1>${project.name}</h1><p>${project.desc}</p>`;
+            container.appendChild(item);
+        });
     })
     .catch(err => {
         console.error('Error loading JSON:', err);
