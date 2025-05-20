73cf0d4896695133dabb19e55b8b4d530c79cc61
Owen Gallegos <owengallegos23@gmail.com>
2025-05-19 21:21:42 -0700

    Fixed navigation bar

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+// script.js
+// Owen Gallegos
 
 
 
@@ -50,3 +52,28 @@ fetch('../content/index-content.json')
     .catch(err => {
         console.error('Error loading JSON:', err);
     });
+
+
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-direct");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+
+        if (scrollY >= sectionTop - sectionHeight / 3) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach((link) => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === `#${current}`) {
+            link.classList.add("active");
+        }
+    });
+});
